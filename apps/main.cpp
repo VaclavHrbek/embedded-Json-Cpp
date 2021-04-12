91d43d7f43822643e226265f9fa35ce74f4dfacf
vaclav.hrbek <vaclav.hrbek@sanezoo.com>
2021-04-12 15:01:56 +0200

    Add load json function

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "lib.cpp"
+#include <filesystem>
+#include <nlohmann/json.hpp>
+
+// for convenience
+using json = nlohmann::json;
 
 int main(){
     std::cout << "Hello from program";
+    std::filesystem::path path{"../data/data.json"} ;
+    json j = Json::loadJson(path);
     return 0;
+
 }
--- src/lib.cpp
+++ src/lib.cpp
@@ -1,4 +1,23 @@
 #include <nlohmann/json.hpp>
+#include <filesystem>
+#include <iostream>
+#include <fstream>
 
 // for convenience
 using json = nlohmann::json;
+
+
+namespace Json 
+{
+    json loadJson(const std::filesystem::path &path){
+
+        // read a JSON file
+        std::ifstream i(path);
+        json j;
+        i >> j;
+        return j;
+    }
+
+
+
+}
